8fbc9973a3601e3d0baf116df77a5569a72776e6
Gamma <37131904+versiongamma@users.noreply.github.com>
2023-06-19 22:35:54 +1200

    Move header style back to sx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,27 +1,28 @@
 import useScreenResolution from "@/hooks/use-screen-resolution";
 import { Toolbar } from "@mui/material";
 import { styled } from "goober";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import Login from "./login";
-import MobileNav from "./mobile-nav";
 import DesktopNav from "./desktop-nav";
+import MobileNav from "./mobile-nav";
 
 const StyledToolbar = styled(Toolbar)`
   display: flex;
   position: fixed;
   justify-content: space-between;
   z-index: 1;
-  background-color: rgba(0, 0, 0, 0.2);
-  width: 100vw;
-  backdrop-filter: blur(1rem);
 `;
 
 const Header = () => {
   const { isMobileResolution } = useScreenResolution();
 
   return (
-    <StyledToolbar sx={{}}>
+    <StyledToolbar
+      sx={{
+        backgroundColor: "rgba(0,0,0,0.2)",
+        position: "fixed",
+        width: "100vw",
+        backdropFilter: "blur(1rem)",
+      }}
+    >
       {isMobileResolution && <MobileNav />}
       {!isMobileResolution && <DesktopNav />}
     </StyledToolbar>
